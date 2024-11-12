9ae05bad0a382d1928b1bf660d7a4fedb7156ea7
DavidH1ll <david@pixelpro.me.uk>
2024-11-12 13:44:27 +0000

    first commit

--- Day 1/tasks.py
+++ Day 1/tasks.py
@@ -1,1 +1,2 @@
-print("Hello" + " David")
+# Description: This is a simple python program that takes a user's name as input and prints "Hello <name>!" to the console.
+print("Hello " + input('Enter your name: ')+"!")
