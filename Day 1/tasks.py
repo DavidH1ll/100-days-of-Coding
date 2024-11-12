c1a1921ad122a333e78a132293ca919a8877e5a9
DavidH1ll <david@pixelpro.me.uk>
2024-11-12 17:01:37 +0000

    final commit

--- Day 1/tasks.py
+++ Day 1/tasks.py
@@ -1,2 +1,5 @@
 # Description: This is a simple python program that takes a user's name as input and prints "Hello <name>!" to the console.
-print("Hello " + input('Enter your name: ')+"!")
+print(f"Welcome to the band name Generator.")
+city = input('whats the name of the city you grew up in: \n')
+pet = input('whats your pets name: \n')
+print(f"Your band name could be: {city} {pet}")
