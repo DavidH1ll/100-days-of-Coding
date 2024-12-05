86f8ef0072a19c2d890655c59079522d3efe1b22
DavidH1ll <186690520+DavidH1ll@users.noreply.github.com>
2024-12-05 13:53:55 +0000

    sat 13 complete

---
+++ Day 13/exercise.py
@@ -0,0 +1,15 @@
+# Target is the number up to which we count
+
+def fizz_buzz(target):
+    for number in range(1, target + 1):
+        if number % 3 == 0 and number % 5 == 0:
+            print("FizzBuzz")
+        elif number % 3 == 0:
+            print("Fizz")  # Changed from "Fizz"
+        elif number % 5 == 0:
+            print("Buzz")  # Changed from "Buzz"
+        else:
+            print(number)  # Added square brackets
+
+# Test the function
+fizz_buzz(15)
