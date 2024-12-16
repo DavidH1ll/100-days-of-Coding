d4b6f18cf906f00a34c7c314d9bc45152a7dd055
DavidH1ll <186690520+DavidH1ll@users.noreply.github.com>
2024-12-16 17:00:39 +0000

    Re organised file system to work better for file paths

---
+++ Day020/score.py
@@ -0,0 +1,48 @@
+from turtle import Turtle
+
+class Score(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.high_score = self.read_high_score()
+        self.color("white")
+        self.penup()
+        self.hideturtle()
+        self.goto(0, 260)
+        self.update_score()
+
+    def read_high_score(self):
+        try:
+            with open("data.txt", "r") as file:
+                return int(file.read())
+        except FileNotFoundError:
+            return 0
+
+    def save_high_score(self):
+        with open("data.txt", "w") as file:
+            file.write(str(self.high_score))
+
+    def update_score(self):
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.high_score}", align="center", font=("Arial", 24, "normal"))
+
+    def increase_score(self):
+        self.score += 1
+        if self.score > self.high_score:
+            self.high_score = self.score
+            self.save_high_score()
+        self.update_score()
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            self.save_high_score()
+        self.score = 0
+        self.goto(0, 260)
+        self.update_score()
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("GAME OVER", align="center", font=("Arial", 24, "normal"))
+        self.goto(0, -30)
+        self.write("Press Enter to Restart", align="center", font=("Arial", 18, "normal"))
